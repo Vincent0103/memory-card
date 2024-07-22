93b67b1c0cb40424756acdae5cd28d5892e73754
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-07-22 21:05:48 +0200

    Setup repo

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        'custom-text': 'rgba(255, 255, 255, 0.87)',
+        'custom-background': '#242424',
+      },
+    },
+  },
+  plugins: [],
+}
+
