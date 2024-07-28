3b86e095cf9d60b6549317885e42abdfd798e3e2
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-07-28 12:20:13 +0200

    Add Card ability to return down

--- src/components/Gameboard/Card.jsx
+++ src/components/Gameboard/Card.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Tilt from "react-parallax-tilt";
 
-const Card = ({ characterId }) => {
+const Card = ({ handleCardClick, isCardClicked, characterId }) => {
   const [characterImgs, setCharacterImgs] = useState({
     name: null,
     jpg: null,
@@ -31,23 +31,35 @@ const Card = ({ characterId }) => {
   }, []);
 
   return (
-    <div className="row-start-2">
+    <div className="row-start-2" onClick={handleCardClick}>
       <Tilt
-        glareEnable={true}
-        glareMaxOpacity={0.8}
+        glareEnable={!isCardClicked}
+        glareMaxOpacity={.5}
         glareColor="#ffffff"
         glarePosition="bottom"
         glareBorderRadius="6px"
       >
-        <div className="w-48 h-72 rounded-md shadow-lg bg-red-100 overflow-hidden border-2 border-black">
-          <picture>
-            <source srcSet={characterImgs.webp} type="image/webp" />
-            <img
-              className="h-full w-full object-cover"
-              src={characterImgs.jpg}
-              alt={`Picture of ${characterImgs.name}`}
-            />
-          </picture>
+        <div className="relative">
+          <div className={`w-48 h-72 rounded-md shadow-lg bg-red-100 overflow-hidden border-2 border-black
+          relative preserve-3d transition-transform-slowly ${isCardClicked ? "return-down" : ""}
+          hide-backface`}>
+            <picture>
+              <source srcSet={characterImgs.webp} type="image/webp" />
+              <img
+                className="h-full w-full object-cover"
+                src={characterImgs.jpg}
+                alt={`Picture of ${characterImgs.name}`}
+              />
+            </picture>
+          </div>
+          <div className={`w-48 h-72 rounded-md shadow-lg bg-gradient-to-br from-zinc-700 to-zinc-900 overflow-hidden border-2 border-black
+          absolute top-0 left-0 preserve-3d transition-transform-slowly ${isCardClicked ? "" : "return-down"}
+          hide-backface`}>
+            <h1 className="font-divergentes text-8xl absolute top-[90px] left-[60px] z-[4] text-white">M</h1>
+            <h1 className="font-divergentes text-8xl absolute top-[95px] left-[65px] z-[3] text-black/50">M</h1>
+            <h1 className="font-divergentes text-8xl absolute top-[120px] left-[95px] z-[2] text-white">C</h1>
+            <h1 className="font-divergentes text-8xl absolute top-[125px] left-[100px] z-[1] text-black/50">C</h1>
+          </div>
         </div>
       </Tilt>
     </div>
--- src/components/Gameboard/Gameboard.jsx
+++ src/components/Gameboard/Gameboard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Card from "./Card";
 
 const CardsContainer = ({ hasGameStarted, children }) => {
@@ -17,11 +18,18 @@ const CardsContainer = ({ hasGameStarted, children }) => {
 
 const Gameboard = ({ hasGameStarted }) => {
   const characterIds = [170732, 170733, 170734, 170735, 174749, 174750, 174748];
+  const [isCardClicked, setIsCardClicked] = useState(false);
+
+  const handleCardClick = () => {
+    setIsCardClicked(!isCardClicked);
+  }
 
   return (
     <CardsContainer hasGameStarted={hasGameStarted}>
       {characterIds.map((id, index) => (
-        <Card key={index} characterId={id} />
+        <Card key={index} characterId={id}
+        isCardClicked={isCardClicked}
+        handleCardClick={handleCardClick} />
       ))}
     </CardsContainer>
   );
